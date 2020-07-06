ee67111f2f907b1965cb115b8fc28bae80618da0
abhishekgupta930 <agupta38@ncsu.edu>
2020-07-06 13:42:14 -0400

    Tackle Thread Termination

--- threadsafeHash.c
+++ threadsafeHash.c
@@ -5,7 +5,7 @@
  *      Author: sonum
  */
 
-#define CAPACITY 5
+#define CAPACITY 100
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -14,7 +14,7 @@
 #include <unistd.h>
 
 
-const int MAX = 27; 
+//const int MAX = 27; 
 
 //Key value pair
 
@@ -23,12 +23,49 @@ const int MAX = 27;
 pthread_mutex_t lock[CAPACITY];
 
 
+// Get's random portNo (value in HashTable)for automated testing
+int getRandom(int lower, int upper) 
+{ 
+    int num = (rand() % (upper - lower + 1)) + lower; 
+    //printf("%d ",num);
+    return num;
+    
+}
+
+//Get random string for VLAN (key in Hashtable)
+char* getRandomString() 
+{ 
+      
+    int i;
+    static const char alphanum[] =     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *s = (char*)malloc(6*sizeof(char));
+	for (i = 0; i <5; ++i) {
+	        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+	}
+	
+	s[5] = '\0';
+	//printf("%s ",s);
+	return s;
+
+}
+
+// Data Structure to be passed as thread parameter 
+typedef struct threadData{
+
+	char vlan[20];
+	int portNo;
+	
+}threadData;
+
+
+// Data Structure for individual bucket in HashTable
 typedef struct Bucket{
  	char * key;
  	int val;
 	struct Bucket* next;
 }bucket;
 
+// Data Structure for HashTable
 typedef struct Hashtable
 {
 	bucket ** bucket_ptr_arr;
@@ -36,9 +73,11 @@ typedef struct Hashtable
 
 }hashtable;
 
+// Hash Table array
 hashtable* table_ptr_arr[5];
 
 
+// Creates a new Bucket 
 bucket* create_bucket(const char * key, int val)
 {
  	//printf("\nIn create bucket");
@@ -51,6 +90,7 @@ bucket* create_bucket(const char * key, int val)
 }
 
 
+// Creates a new HashTable 
 hashtable * createHashTable(int size)
 {
 
@@ -59,8 +99,8 @@ hashtable * createHashTable(int size)
  hashtable* ht = (hashtable*) malloc(sizeof(hashtable)) ;
  ht->bucket_ptr_arr = (bucket **)malloc(size*(sizeof(bucket *)));
  ht->size = size;
+ 
  // Initialize all buckets to null
-
  for (i=0 ; i < ht->size; i++)
  {
 
@@ -71,6 +111,8 @@ hashtable * createHashTable(int size)
  return ht;
 }
 
+
+// Simple Hash Fucntion
 int hashfunction(const char* key)
 {
 
@@ -87,35 +129,34 @@ int hashfunction(const char* key)
 }
 
 
-void * insert(void* input)
+void insert(bucket* input)
 
 {
 
-	//printf("\nIn function insert() .........thread id = %d", pthread_self());
+	// printf("\nIn function insert() .........thread id = %d", pthread_self());
 
 	bucket* bucket_ptr = (bucket *) input;
 
 	bucket* ptr = NULL;
 	bucket* trav = NULL;
 
-    //Obtain index using hashfunction
+        //Obtain index using hashfunction
 	int index = hashfunction(bucket_ptr->key);
-
+	//printf("\nkey =  %s , value = %d , index = %d , thread id = %ld", bucket_ptr->key,bucket_ptr->val, index, pthread_self());
 	//Created item {key-value pair}
 
 
 	// Critical section
 
-    pthread_mutex_lock(&lock[index]);
+        pthread_mutex_lock(&lock[index]);
 
- 	//printf("\nThread %d has acuired the lock %d",pthread_self(),lock[index]);
+ 	printf("\nThread %ld has acuired the lock %d",pthread_self(),index);
 	//printf("\nIn ....Critical Section.... Thread id = %d", pthread_self());
 	
 	
 	// Sleep emulates internal processsing delays to test thread-safety
-	//sleep(getRandom(1,5));
+	sleep(getRandom(1,4));
 
-	//	sleep(20);
 
 
 	//bucket is empty. Therefore insert right away
@@ -133,11 +174,10 @@ void * insert(void* input)
 	  		    if (strcmp(bucket_ptr->key,ptr->key)==0)
 	  	 	        {
 			            ptr->val= bucket_ptr->val;
- 						//printf("\nThread %d has released the lock %d",pthread_self(), lock[index]);
-						//printf("\n Exit....Critical Section.... Thread id = %d\n", pthread_self());
-						pthread_exit(NULL);
-			            return NULL;	            
-
+ 				    //printf("\nThread %d has released the lock %d",pthread_self(), lock[index]);
+				    //printf("\n Exit....Critical Section.... Thread id = %d\n", pthread_self());	            
+			            pthread_mutex_unlock(&lock[index]);
+						
 			        }
 
 	  		    else
@@ -161,9 +201,6 @@ void * insert(void* input)
  	//printf("\nThread %d has released the lock %d",pthread_self(), lock[index]);
 	//printf("\n Exit....Critical Section.... Thread id = %d\n", pthread_self());
 
-
-	pthread_exit(NULL);
-	return NULL;
 }
 
 int findValue(hashtable * table, const char *key)
@@ -200,32 +237,26 @@ int findValue(hashtable * table, const char *key)
 
 }
 
-void add(void)
+// Adds Key Value pair in HashTable
+
+void* add(void* targ)
  {
- 		char vlan[20];
- 		int portNo;
- 		bucket*  input;
- 		pthread_t writeT;
+ 	threadData * mydata = (threadData *) targ;		
+	bucket*  input;
+	//printf("before create bucket");
+	input = create_bucket(mydata->vlan, mydata->portNo );
+	//printf("\nAdd function key =  %s , value = %d , thread id = %ld", input->key,input->val, pthread_self());
+	insert(input);
+}
 
-		printf("Enter the Vlan Name\n");
-		scanf("%s", &vlan);
-
-		printf("Enter the Port No. \n");
-		scanf("%d", &portNo);
-
-
-		input = create_bucket(vlan, portNo );
-		pthread_create(&writeT, NULL, insert, (void*)input);
-		
-	}
-
+// Displays specific entry (Key-Value pair)in Hashtable
 
 void display()
 {
 	pthread_t readT;
 	char vlan[20];
 	printf("Enter the Vlan Name\n");
-	scanf("%s", &vlan);
+	scanf("%s", vlan);
 	printf("---------------------------------------------");
 	printf("\n Vlan ID =  %s  Port No : portNo %d\n", vlan, findValue(table_ptr_arr[0], vlan));
 	printf("---------------------------------------------\n");
@@ -233,6 +264,7 @@ void display()
 }
 
 
+// Displays complete Hashtable
 
 void displayall()
 {
@@ -247,71 +279,15 @@ void displayall()
 		bucket_ptr = table_ptr_arr[0]->bucket_ptr_arr[index];
 		printf("|%d| : ",index);
 		while (bucket_ptr != NULL)
-	        {
-				printf("%s : %d\t",bucket_ptr->key, bucket_ptr->val);
-                bucket_ptr = bucket_ptr->next;
-	  	    }
+	           {
+			printf("%s : %d\t",bucket_ptr->key, bucket_ptr->val);
+                	bucket_ptr = bucket_ptr->next;
+	  	   }
 	  	printf("\n");
 	}
 	
 }
 
-
-int getRandom(int lower, int upper) 
-{ 
-    int num = (rand() % (upper - lower + 1)) + lower; 
-    //printf("%d ",num);
-    return num;
-    
-}
-
-
-char* getRandomString() 
-{ 
-      
-    int i;
-    static const char alphanum[] =     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *s = (char*)malloc(5*sizeof(char));
-	for (i = 0; i <5; ++i) {
-	        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-	}
-	
-	s[5] = '\0';
-	//printf("%s ",s);
-	return s;
-
-}
-// We will make this hash implementation thread safe
-
-void automatedTesting()
-{
-	
-	char* vlan;
- 	int portNo;
- 	bucket*  input;
- 	int i=0;
-	printf("\nGenerating Following Test Cases");
-	printf("\n-----------------------------------");
-
-	for (i=0;i<2*CAPACITY; i++){
-		pthread_t writeT;
-		vlan = (char*)malloc(5*sizeof(char));
-		strcpy(vlan,getRandomString());
-		portNo= getRandom(2000,8000);
-		printf("\n VLan: %s | PortNo: %d", vlan, portNo);
-		input = create_bucket(vlan, portNo );
-		pthread_create(&writeT, NULL, insert, (void*)input);
-	}
-	printf("\n-----------------------------------");
-
-	
-	for (i = 0; i<CAPACITY; i++)
-	{
-		sleep(2);
-		displayall();
-	}
-	  
-}
 
 
 void freeHashTable (void)
@@ -322,36 +298,91 @@ void freeHashTable (void)
 
 	int index = 0;
 	bucket* bucket_ptr = NULL;
+	bucket* tmp = NULL; 
 	
 	for (index=0; index<CAPACITY; index++)
 	{
 		bucket_ptr = table_ptr_arr[0]->bucket_ptr_arr[index];
 		while (bucket_ptr != NULL)
 	        {
-				free(bucket_ptr);
-                bucket_ptr = bucket_ptr->next;
-	  	    }
+			printf("Freeing bucket %s : %d\t",bucket_ptr->key, bucket_ptr->val);			
+			tmp = bucket_ptr;
+			bucket_ptr = bucket_ptr->next;
+			free(tmp->key);
+			free(tmp);
+	  	}
+
+	  	printf("\n");
 		// may be free individual buckets
-		free(bucket_ptr);
+		//free(bucket_ptr);
 	
 	}
 	
-	
+
+	// free all the bucket pointers of hash table
+	free(table_ptr_arr[0]->bucket_ptr_arr);
 	
 	// free the complete hash table
-	
 	free(table_ptr_arr[0]);
-	
+
+
+
 	
 }
+// We will make this hash implementation thread safe
+
+void automatedTesting()
+{
+	
+	char * randomString = NULL;
+ 	bucket*  input = NULL;
+	pthread_t writeT[2*CAPACITY];
+	threadData tData[2*CAPACITY];
+ 	int i=0;
+	printf("\nGenerating Following Test Cases");
+	printf("\n-----------------------------------");
+
+	for (i=0;i<2*CAPACITY; i++){
+		randomString = getRandomString();
+		strcpy(tData[i].vlan,randomString);
+		tData[i].portNo= getRandom(2000,8000);
+		printf("\n VLan: %s | PortNo: %d", tData[i].vlan, tData[i].portNo);
+		pthread_create(&writeT[i], NULL, add, (void*)&tData[i]);
+		free(randomString);
+	}
+	printf("\n-----------------------------------");
+
+	// Displaying Complete HasTable	
+	for (i = 0; i<2*CAPACITY; i++)
+	{
+		sleep(1);
+		printf("-----------Iteration %d", i);
+		displayall();
+	}
+
+
+
+	printf("\nbefore joining threads");
+	for (i=0;i<2*CAPACITY; i++)
+    		pthread_join(writeT[i], NULL); 
+
+	// Free allocated memory from heap
+	freeHashTable();
+	  
+}
+
+
+
+
 
 
 int main()
 {
 	int i,choice;
 	bucket*  input;
-
-
+	threadData tData;
+	int threadCount = -1;
+	pthread_t writeT[2*CAPACITY];
 
 	// Initializing Mutex
 	for (i = 0 ; i<CAPACITY;i++)
@@ -369,13 +400,12 @@ int main()
 	table_ptr_arr[0] = createHashTable(CAPACITY);
 
 	// Test Case
-	
-	
+		
 	
 	//Automated Testing
-	//automatedTesting();
+	automatedTesting();
 	
-	
+	/*
 	//Menu Driven
 	while (1)
 	{
@@ -390,7 +420,16 @@ int main()
 		scanf("%d",&choice);
 		if (choice ==1)
 			{
-				add();
+ 				char vlan[20];
+ 				int portNo;
+				printf("Enter the Vlan Name\n");
+				scanf("%s", tData.vlan);
+
+				printf("Enter the Port No. \n");
+				scanf("%d", &(tData.portNo));
+				threadCount++;	
+				pthread_create(&writeT[threadCount], NULL, add,(void *) &tData);
+				
 			}
 
 		else if (choice ==2)
@@ -416,8 +455,18 @@ int main()
 			}	
 
 	}
+
+	printf("ThreadCount= %d\n",threadCount);
+	for (i=0;i<=threadCount; i++)
+	{
+    		printf("join");
+		pthread_join(writeT[i], NULL); 
+	}
+
+	*/
 	return 0 ;
 
 }
+
 
 
